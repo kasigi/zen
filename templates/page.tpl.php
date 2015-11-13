25fd3118a3e3203ca7ad20dc9e55e018f18ed15f
johnson-tor-boozallen <johnson_tor@bah.com>
2015-11-13 12:59:04 -0500

    Added screenshot and new content block area

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -68,6 +68,7 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
+      <?php print render($page['content_under']); ?>
       <?php print $feed_icons; ?>
     </div>
 
