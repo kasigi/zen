ba500dc69ed44ca5116de88eaf90917d9d9df84b
johnson-tor-boozallen <johnson_tor@bah.com>
2015-11-13 16:39:55 -0500

    Updates to gallery field template

--- templates/field--field-project-images.tpl.php
+++ templates/field--field-project-images.tpl.php
@@ -46,11 +46,11 @@
 ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
     <?php if (!$label_hidden): ?>
-        <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+        <div class="tnj-corestyles tnj-Gallery-Title field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
     <?php endif; ?>
     <div class="gallery tnj-Gallery isotope field-items"<?php print $content_attributes; ?>>
         <?php foreach ($items as $delta => $item): ?>
-            <div class="tnj-GalleryElement field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+            <div class="tnj-GalleryElement  tnj-GalleryThumb isotope-item field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
         <?php endforeach; ?>
     </div>
 </div>
